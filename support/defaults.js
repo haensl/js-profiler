e8b342f588ed9c2183d6acb1e86ad33f6eb18313
HP Dietz <hp@jandh.org>
2017-02-08 23:24:16 +0100

    Add command line options and arguments.   - help   - iterations   - verbosity

---
+++ support/defaults.js
@@ -0,0 +1,7 @@
+'use strict';
+const join = require('path').join;
+const VERBOSITY = require(join(__appRoot, 'support/verbosity'));
+module.exports = {
+  iterations: 1000,
+  verbosity: VERBOSITY.DEFAULT
+};
---
+++ support/verbosity.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  QUIET: 0,
+  DEFAULT: 1,
+  VERBOSE: 2
+};
